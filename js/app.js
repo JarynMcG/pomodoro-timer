668db10a93b7ea6e70358fa603431698ff632905
Jerron <jerron.mcginnis@gmail.com>
2020-11-15 13:28:07 -0500

    Initial Javascript for functionality of adding list items

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+
+function addTodo(event) {
+    //Prevents form from submitting
+    event.preventDefault();
+    //Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Done button
+    const doneButton = document.createElement('button');
+    doneButton.innerHTML = '<i class="fas fa-check"></i>';
+    doneButton.classList.add("done-btn");
+    todoDiv.appendChild(doneButton);
+    //Delete button
+    const deleteButton = document.createElement('button');     
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear input value
+    todoInput.value = "";
+
+}
